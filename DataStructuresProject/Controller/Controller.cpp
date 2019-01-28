0aa853035ddac050c0426a777e6face9a0bd741f
Spencer B <spencerbillings2000@gmail.com>
2019-01-28 09:25:34 -0700

    Wrote the runner, controller, and node files

---
+++ DataStructuresProject/Controller/Controller.cpp
@@ -0,0 +1,14 @@
+//
+//  Controller.cpp
+//  DataStructuresProject
+//
+//  Created by Billings, Spencer on 1/28/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#include "Controller.hpp"
+
+void Controller :: start()
+{
+    cout << "Welcome, welcome to Papa's house." << endl;
+}
