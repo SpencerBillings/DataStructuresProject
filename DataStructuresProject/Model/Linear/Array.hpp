4024faf7561ca642e168640e56dd522710a17797
Spencer B <spencerbillings2000@gmail.com>
2019-02-07 08:24:50 -0700

    Created the List and Array header files

---
+++ DataStructuresProject/Model/Linear/Array.hpp
@@ -0,0 +1,13 @@
+//
+//  Array.h
+//  DataStructuresProject
+//
+//  Created by Billings, Spencer on 2/7/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef Array_hpp
+#define Array_hpp
+
+
+#endif /* Array_hpp */
---
+++ DataStructuresProject/Model/Linear/List.hpp
@@ -0,0 +1,13 @@
+//
+//  List.hpp
+//  DataStructuresProject
+//
+//  Created by Billings, Spencer on 2/7/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef List_hpp
+#define List_hpp
+
+
+#endif /* List_hpp */
