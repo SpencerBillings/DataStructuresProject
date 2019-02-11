446588015d755b53084167d6dbc0f21c0daddeb9
Spencer B <spencerbillings2000@gmail.com>
2019-02-11 09:42:56 -0700

    Made progress on the Queue header file

--- DataStructuresProject/Model/Linear/Queue.hpp
+++ DataStructuresProject/Model/Linear/Queue.hpp
@@ -17,6 +17,7 @@ using namespace std;
 template <class Type>
 class class Queue : public LinkedList<Type>
 {
+public:
     Queue();
     ~Queue();
     
@@ -26,8 +27,65 @@ class class Queue : public LinkedList<Type>
     Type peek();
     void clear();
     
-public:
-    <#member functions#>
+    // Overridden LinkedList methods
+    void add(Type data);
+    void addAtIndex(int index, Type data);
+    Type getFromIndex(int index);
+    Type remove(int index);
 };
+
+template <class Type>
+Queue <Type> :: Queue() : LinkedList<Type>()
+{
+    // Empty (handeled by the parent class constructor)
+}
+
+template <class Type>
+Queue<Type> :: ~Queue()
+{
+    for (LinearNode<Type> * removed = this->front; removed != nullptr; removed = this->front)
+    {
+        this->front = removed->getNextNode();
+        delete removed;
+    }
+}
+
+template <class Type>
+void Queue<Type> :: enqueue(Type item)
+{
+    LinearNode<Type> * added = new LinearNode<Type>(item);
+    
+    if (this->size == 0)
+    {
+        this->front = added;
+    }
+    else
+    {
+        this->end->setNextNode(added);
+    }
+    
+    this->end = added;
+    this->size++;
+}
+
+template <class Type>
+void Queue<Type> :: addAtFunction(int index, Type item)
+{
+    assert(index == this->size);
+    enqueue(item);
+}
+
+template <class Type>
+void Queue<Type> :: add(Type item)
+void Queue<Type> :: add(Type item)
+{
+    enqueue(item);
+}
+
+template <class Type>
+Type Queue<Type> :: dequeue()
+{
+    <#statements#>
+}
 
 #endif /* Queue_hpp */
