5e6c98f40fbe9580e317b1dfdddc1163fab6efb1
Spencer B <spencerbillings2000@gmail.com>
2019-04-12 08:55:02 -0600

    Created BinaryTreeNode in the Node folder

---
+++ DataStructuresProject/Model/Nodes/BinaryTreeNode.hpp
@@ -0,0 +1,86 @@
+//
+//  BinaryTreeNode.hpp
+//  DataStructuresProject
+//
+//  Created by Billings, Spencer on 4/12/19.
+//  Copyright © 2019 CTEC. All rights reserved.
+//
+
+#ifndef BinaryTreeNode_hpp
+#define BinaryTreeNode_hpp
+
+#include "Node.hpp"
+
+template <class Type>
+class BinaryTreeNode : public Node<Type>
+{
+private:
+    BinaryTreeNode<Type> * root;
+    BinaryTreeNode<Type> * leftChild;
+    BinaryTreeNode<Type> * rightChild;
+    
+public:
+    BinaryTreeNode();
+    BinaryTreeNode(Type data);
+    
+    BinaryTreeNode<Type> * getRoot();
+    BinaryTreeNode<Type> * getLeftChild();
+    BinaryTreeNode<Type> * getRightChild();
+    
+    void setRoot(BinaryTreeNode<Type> * root);
+    void setLeftChild(BinaryTreeNode<Type> * leftChild);
+    void setRightChild(BinaryTreeNode<Type> * rightChild);
+    
+};
+
+template <class Type>
+BinaryTreeNode<Type> :: BinaryTreeNode() : Node<Type>()
+{
+
+}
+
+template <class Type>
+BinaryTreeNode<Type> :: BinaryTreeNode(Type data) : Node<Type>(data)
+{
+    this->root = nullptr;
+    this->leftChild = nullptr;
+    this->rightChild = nullptr;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRoot()
+{
+    return root;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getLeftChild()
+{
+    return leftChild;
+}
+
+template <class Type>
+BinaryTreeNode<Type> * BinaryTreeNode<Type> :: getRightChild()
+{
+    return rightChild;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setRoot(BinaryTreeNode<Type> * root)
+{
+    this->root = root;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setLeftChild(BinaryTreeNode<Type> * leftChild)
+{
+    this->leftChild = leftChild;
+}
+
+template <class Type>
+void BinaryTreeNode<Type> :: setRightChild(BinaryTreeNode<Type> * rightChild)
+{
+    this->rightChild = rightChild;
+}
+
+#endif /* BinaryTreeNode_hpp */
